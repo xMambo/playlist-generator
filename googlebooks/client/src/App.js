0520193be460c20d2f4e426b44ead2499fdef7a8
xMambo <millerdesigncotn@gmail.com>
2019-06-14 19:53:26 -0500

    got a successful get request from google books api

--- googlebooks/client/src/App.js
+++ googlebooks/client/src/App.js
@@ -5,7 +5,8 @@ import Input from "./components/Input";
 import Button from "./components/Button";
 import { BookList, BookListItem } from "./components/BookList";
 import { Container, Row, Col } from "./components/Grid";
-import API from "./utils/API";
+import axios from "axios";
+//import API from "./utils/API";
 
 
 class App extends Component {
@@ -24,13 +25,17 @@ class App extends Component {
     });
   };
 
-handleFormSubmit = event => {
-    // When the form is submitted, prevent its default behavior, get recipes update the recipes state
-  event.preventDefault();
-  API.getBooks(this.state.bookSearch)
-    .then(res => this.setState({ books: res.data }))
-    .catch(err => console.log(err));
-};
+  handleFormSubmit = (event) => {
+		console.log('firing');
+		event.preventDefault();
+		return axios
+			.get('https://www.googleapis.com/books/v1/volumes?q=' + this.state.title)
+			.then((response) => {
+				console.log(response);
+				this.setState({ books: response.data.items });
+			})
+			.catch((err) => console.log(err));
+	};
 
 render() {
   return (
@@ -71,13 +76,12 @@ render() {
               <h1 className="text-center">No Book to Display</h1>
             ) : (
               <BookList>
-                {this.state.recipes.map(book => {
+                {this.state.books.map(book => {
                   return (
                     <BookListItem
                       key={book.title}
                       title={book.title}
                       href={book.href}
-                      ingredients={book.ingredients}
                       thumbnail={book.thumbnail}
                     />
                   );
