d0d547239cc15b9890f6013d17fabdc0ac0adeb8
xMambo <millerdesigncotn@gmail.com>
2019-06-22 02:59:50 -0500

    Updates

---
+++ precache-manifest.09810e554bfb19bd7cf45dd3813d64b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fad0f5a99e05db1340e2f00ba909f4aa",
+    "url": "/playlist-generator/index.html"
+  },
+  {
+    "revision": "31b83ebb4717539a5f44",
+    "url": "/playlist-generator/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/playlist-generator/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "31b83ebb4717539a5f44",
+    "url": "/playlist-generator/static/js/main.1ccf7d26.chunk.js"
+  },
+  {
+    "revision": "b1c30b5e2803adb56176",
+    "url": "/playlist-generator/static/js/runtime~main.924cc3f2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/playlist-generator/static/media/logo.5d5d9eef.svg"
+  }
+]);
